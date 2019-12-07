70581774faea9d4d3de5a3ed51693a7a993abe94
c1tr00z <0range.br4in@gmail.com>
2019-12-07 18:05:52 +0900

    https://github.com/c1tr00z/assist-lib/commit/000c7cae370eecdb0ed8564088a9aa5bd6be8b51 https://github.com/c1tr00z/assist-lib/commit/fa7c50da722d0df33a1e94ab2934d7fab2e18fe2 https://github.com/c1tr00z/assist-lib/commit/0c1dd0d0d63811c466d620422bd336b4a58c2d9b https://github.com/c1tr00z/assist-lib/commit/401c7fb514792d78c69216a3e4b45a13297273e6 https://github.com/c1tr00z/assist-lib/commit/2a1e6afb03457088b301df4a4f3630eb9a23f133 https://github.com/c1tr00z/assist-lib/commit/2109335162eca0391b800b8f832e838ef3f2d1ee https://github.com/c1tr00z/assist-lib/commit/203293669562b499351e50742c8dcd572f5295f5 https://github.com/c1tr00z/assist-lib/commit/ad5905c2d7215246a235935b89701bdb91949dd7 https://github.com/c1tr00z/assist-lib/commit/753c4135d5f16a7012f13522115d1189c9193c08 https://github.com/c1tr00z/assist-lib/commit/2ee7b726af575d832ed01d1c37f3a5edf5176307

---
+++ Editor/DBEntriesEditorEvents.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace AssistLib.Editor {
+    [InitializeOnLoad]
+    public class DBEntriesEditorEvents {
+        public DBEntriesEditorEvents() {
+            EditorApplication.projectChanged += EditorApplicationOnProjectChanged;
+            ItemsEditor.CollectItems();
+        }
+
+        private void EditorApplicationOnProjectChanged() {
+            ItemsEditor.CollectItems();
+        }
+    }
+}
