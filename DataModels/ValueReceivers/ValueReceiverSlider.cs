5633708669a92935f0d86f8527ff71fa7a05e096
c1tr00z <c1tr00z@yandex.ru>
2020-05-03 19:34:15 +0900

    Add Value Receiver Slider

---
+++ DataModels/ValueReceivers/ValueReceiverSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using c1tr00z.AssistLib.PropertyReferences;
+using UnityEngine.UI;
+
+namespace c1tr00z.AssistLib.DataModels {
+    public class ValueReceiverSlider : ValueReceiverBase {
+        public override bool isRecieverEnabled => base.isRecieverEnabled && slider != null;
+
+        [ReferenceType(typeof(float))]
+        public PropertyReference valueSrc;
+
+        public Slider slider;
+
+        public override void UpdateReceiver() {
+            slider.value = valueSrc.Get<float>();
+        }
+        
+        public override IEnumerator<PropertyReference> GetReferences() {
+            yield return valueSrc;
+        }
+    }
+}
