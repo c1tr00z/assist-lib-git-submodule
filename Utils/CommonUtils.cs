a93408e36fe3c6d7f363033fb6b608e72dfc296e
c1tr00z <0range.br4in@gmail.com>
2020-07-11 15:37:16 +0900

    Common utils

---
+++ Utils/CommonUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace c1tr00z.AssistLib.Utils {
+    public static class CommonUtils {
+        public static T GetCachedObject<T>(ref T obj, Func<T> getter) {
+            if (obj == null && getter != null) {
+                obj = getter != null ? getter() : default(T);
+            }
+
+            return obj;
+        }
+    }
+}
