0757cedc345aa89dc81bd54a86a441e693f51b27
c1tr00z <c1tr00z@yandex.ru>
2021-02-04 12:16:54 +0900

    Trigger events

---
+++ Gameplay/Code/TriggerEvents.cs
@@ -0,0 +1,42 @@
+using System;
+using c1tr00z.AssistLib.Utils;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+namespace c1tr00z.AssistLib.Gameplay {
+    public class TriggerEvents : MonoBehaviour {
+
+        #region Nested Classes
+
+        [Serializable]
+        private class TriggerEvent : UnityEvent<Collider> {
+        }
+
+        #endregion
+        
+        #region Serialized Fields
+
+        [SerializeField] private TriggerEvent _onTriggerEnterEvent;
+        
+        [SerializeField] private TriggerEvent _onTriggerExitEvent;
+
+        #endregion
+
+        #region Unity Events
+
+        private void OnTriggerEnter(Collider other) {
+            if (_onTriggerEnterEvent.IsAssigned()) {
+                _onTriggerEnterEvent.Invoke(other);
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (_onTriggerExitEvent.IsAssigned()) {
+                _onTriggerExitEvent.Invoke(other);
+            }
+        }
+
+        #endregion
+    }
+}
