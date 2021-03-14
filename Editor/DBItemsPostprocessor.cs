bc05bc142002006fa60fb6e7279e4b4ddb312916
c1tr00z <c1tr00z@yandex.ru>
2021-03-14 21:17:16 +0900

    Temporarily remove dbentry post import processing

--- Editor/DBItemsPostprocessor.cs
+++ Editor/DBItemsPostprocessor.cs
@@ -5,21 +5,21 @@ namespace c1tr00z.AssistLib.ResourceManagement.Editor {
 
     public class DBItemsPostprocessor : AssetPostprocessor {
 
-        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets,
-            string[] movedAssets,
-            string[] movedFromAssetPaths) {
-            var reCollectDB = false;
-
-            reCollectDB =
-                ContainsAsset(importedAssets) || ContainsAsset(deletedAssets) || ContainsAsset(movedAssets);
-
-            if (reCollectDB) {
-                DBEntryEditorUtils.AutoCollect();
-            }
-        }
-
-        private static bool ContainsAsset(string[] paths) {
-            return paths.Any(p => p.Contains("Resources") && (p.ToLower().EndsWith(".asset") || p.Contains("@")));
-        }
+        // private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets,
+        //     string[] movedAssets,
+        //     string[] movedFromAssetPaths) {
+        //     var reCollectDB = false;
+        //
+        //     reCollectDB =
+        //         ContainsAsset(importedAssets) || ContainsAsset(deletedAssets) || ContainsAsset(movedAssets);
+        //
+        //     if (reCollectDB) {
+        //         DBEntryEditorUtils.AutoCollect();
+        //     }
+        // }
+        //
+        // private static bool ContainsAsset(string[] paths) {
+        //     return paths.Any(p => p.Contains("Resources") && (p.ToLower().EndsWith(".asset") || p.Contains("@")));
+        // }
     }
 }
