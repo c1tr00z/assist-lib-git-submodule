850d356b51a1c21266570c6dff4d3f08abc7ac2c
c1tr00z <c1tr00z@yandex.ru>
2022-01-13 22:35:35 +0900

    EditorGUIUtils now support generic method for any object field (not layouted)

--- Utils/Editor/EditorGUIUtils.cs
+++ Utils/Editor/EditorGUIUtils.cs
@@ -1,5 +1,7 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace c1tr00z.AssistLib.Utils {
     public static class EditorGUIUtils {
@@ -16,6 +18,77 @@ namespace c1tr00z.AssistLib.Utils {
 
         public static bool RefreshButton() {
             return GUILayout.Button(EditorGUIUtility.IconContent("TreeEditor.Refresh"), GUILayout.Width(30));
+        }
+
+        public static T Field<T>(Rect rect, string label, T currentValue) {
+            var type = typeof(T);
+
+            return (T)Field(type, rect, label, currentValue);
+        }
+
+        public static object Field(Type type, Rect rect, string label, object currentValue) {
+            if (type == typeof(string)) {
+                var currentString = currentValue.IsNull() ? string.Empty : currentValue.ToString();
+                return EditorGUI.TextField(rect, label, currentString);
+            }
+
+            if (type == typeof(Int32)) {
+                var currentInt = (Int32)currentValue;
+                return EditorGUI.IntField(rect, label, currentInt);
+            }
+            
+            if (type == typeof(Int64)) {
+                var currentLong = (Int64)currentValue;
+                return EditorGUI.LongField(rect, label, currentLong);
+            }
+            
+            if (type == typeof(bool)) {
+                var currentBool = (bool)currentValue;
+                return EditorGUI.Toggle(rect, label, currentBool);
+            }
+
+            if (type == typeof(float)) {
+                var currentFloat = (Single)currentValue;
+                return EditorGUI.FloatField(rect, label, currentFloat);
+            }
+            
+            if (type == typeof(double)) {
+                var currentFloat = (float)currentValue;
+                return EditorGUI.FloatField(rect, label, currentFloat);
+            }
+            
+            if (type == typeof(Vector4)) {
+                var currentVector = (Vector4)currentValue;
+                return EditorGUI.Vector4Field(rect, label, currentVector);
+            }
+            
+            if (type == typeof(Vector3)) {
+                var currentVector = (Vector3)currentValue;
+                return EditorGUI.Vector3Field(rect, label, currentVector);
+            }
+
+            if (type == typeof(Vector2)) {
+                var currentVector = (Vector2)currentValue;
+                return EditorGUI.Vector2Field(rect, label, currentVector);
+            }
+            
+            if (type == typeof(Vector3Int)) {
+                var currentVector = (Vector3Int)currentValue;
+                return EditorGUI.Vector3IntField(rect, label, currentVector);
+            }
+
+            if (type == typeof(Vector2Int)) {
+                var currentVector = (Vector2Int)currentValue;
+                return EditorGUI.Vector2IntField(rect, label, currentVector);
+            }
+            
+            if (type == typeof(Object)) {
+                var currentObject = (Object)currentValue;
+                return EditorGUI.ObjectField(rect, label, currentObject, type, false);
+            }
+            
+            Debug.LogError($"No editor fields for type {type}");
+            return null;
         }
 
         #endregion
